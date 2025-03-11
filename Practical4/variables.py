89a9418d4bf425cd2ea533388e3095aa76493ed2
unknown <298521829@qq.com>
2025-03-11 04:12:09 +0100

    4.1 and 4.2 practice

---
+++ Practical4/variables.py
@@ -0,0 +1,22 @@
+# 4.1
+a=15 #walk to the bus stop
+b=75 #bus journey
+c=a+b #total 1
+d=90 #drive
+e=5 #parking
+f=d+e #total 2
+print("bus:",c,"minutes")
+print("car:",f,"minutes")
+#bus:90minutes car:95minutes 
+#bus is faster
+# 4.2
+X=True
+Y=False
+for X in [True,False]:
+    for Y in [True,False]:
+        W=X and Y
+        print(f"X = {X}, Y = {Y}, W = {W}")
+#X = True, Y = True, W = True
+#X = True, Y = False, W = False
+#X = False, Y = True, W = False
+#X = False, Y = False, W = False
