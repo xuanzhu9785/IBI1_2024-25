d4f16ca05950f08d64cb354aa01d3d2704380e5f
unknown <298521829@qq.com>
2025-03-12 09:55:35 +0100

    my code for a BMI calculator

---
+++ Practical4/bmi_calculator.py
@@ -0,0 +1,10 @@
+weight=float(input("please input your weight(kg):")) #get the person's weight
+height=float(input("please input your height(m):")) #get the person's height
+BMI=weight/(height**2) #calculate the value of BMI
+if BMI<18.5: #decide which category the person's BMI is
+    category="underweight"
+if BMI>30:
+    category="obese"
+else:
+    category="healthy"
+print("The value of your BMI is:",BMI,",Your are",category) #output the result
