49f964e5c65bfc51b73223a27a8ad3bd5a253174
unknown <298521829@qq.com>
2025-03-12 11:37:52 +0100

    updated triangle.py version

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,5 +1,5 @@
 # calculates the triangular sequence
 n=10 #define the circulation to 10 times
 for i in range(1,n+1): # 10 circulations
-    triangle_numbers = i*(i+1)/2 #caculate triangular sequence
+    triangle_numbers = i*(i+1)/2 #calculate triangular sequence
     print(triangle_numbers) #print the result
