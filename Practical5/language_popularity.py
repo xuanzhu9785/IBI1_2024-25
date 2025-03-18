72f9045fb0221063f3c6b6d4f4e3c0713e15cb13
unknown <298521829@qq.com>
2025-03-18 09:41:07 +0100

    popularity of programming languages globally

---
+++ Practical5/language_popularity.py
@@ -0,0 +1,17 @@
+language_popularity={"JavaScript": 62.3,"HTML": 52.9,"Python": 51,"SQL": 51,"TypeScript": 38.5}#define a dictionary of the percentage of 5 programming languages
+print("The statistics about popularity of programming languages",language_popularity)#print the dictionary
+inquire_language="Python"#inquire the percentage of a certain language
+if inquire_language in language_popularity:#determine whether the language is in the dictionary
+    print("The usage of",inquire_language,"is",{language_popularity[inquire_language]},"%")#print the usage of this language
+else:
+    print("The statistic about",inquire_language,"cannot be found")
+
+import matplotlib.pyplot as plt
+import numpy as np
+languages = list(language_popularity.keys())#get the languages
+percentages = list(language_popularity.values())#get the percentages 
+plt.bar(languages, percentages)#create a bar chart
+plt.xlabel("programming languages")#add labels
+plt.ylabel("popularity percentages (%)")
+plt.title("The Popularity of Programming Languages Globally")#add title
+plt.show()
