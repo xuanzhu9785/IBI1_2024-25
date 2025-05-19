40045ccadffcc2e150295249232dc1d1d2c32102
unknown <298521829@qq.com>
2025-05-19 02:57:42 +0100

    the third version

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -8,10 +8,10 @@ f=d+e #total 2
 print("bus:",c,"minutes")
 print("car:",f,"minutes")
 #bus:90minutes car:95minutes 
-is_bus_faster = c < f #compare c with f
-print("Is bus faster than car?", is_bus_faster) #print the result
+compare = c < f #compare c with f
+print("Is bus faster than car?", compare) #print the result
 
-if is_bus_faster: #make a conclusion
+if compare: #make a conclusion
     print("Conclusion: Bus is faster")
 else:
     print("Conclusion: Car is faster or equal")
