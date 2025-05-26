a8d21e9c017d4d1173e42dd9410935e8904975c3
unknown <298521829@qq.com>
2025-05-26 07:49:16 +0100

    second version

--- Practical8/restriction_enzyme_cut_sites.py
+++ Practical8/restriction_enzyme_cut_sites.py
@@ -1,16 +1,28 @@
-def cut_sites(DNA_sequence, enzyme_sequence):
-    bases = {'A', 'C', 'G', 'T'}
-    def validate_sequence(seq, label="DNA"):
-        for base in seq.upper():
-            if base not in bases:
-                raise ValueError(f"{label} sequence contains invalid character: {base}")
-            validate_sequence(DNA_sequence, "DNA")
-            validate_sequence(enzyme_sequence, "Enzyme")
+import re
+def restriction_sites(dna_sequence, enzyme_sequence):
+    
+    if not re.fullmatch(r'[ACGT]+', dna_sequence.upper()):
+        raise ValueError("DNA sequence contains invalid characters. Only A, C, G, T are allowed.")
+    if not re.fullmatch(r'[ACGT]+', enzyme_sequence.upper()):
+        raise ValueError("Enzyme recognition sequence contains invalid characters. Only A, C, G, T are allowed.")
+    if enzyme_sequence not in dna_sequence:
+        raise ValueError("The enzyme sequence is not included in the DNA sequece.")
+    
+    dna_sequence = dna_sequence.upper()
+    enzyme_sequence = enzyme_sequence.upper()
+    
     positions = []
-    for i in range(len(DNA_sequence) - len(enzyme_sequence) + 1):
-        if DNA_sequence[i:i+len(enzyme_sequence)].upper() == enzyme_sequence.upper():
-            positions.append(i)
-
+    for i in range(len(dna_sequence) - len(enzyme_sequence) + 1):
+        if dna_sequence[i:i + len(enzyme_sequence)] == enzyme_sequence:
+            positions.append(i+1)
+    
     return positions
 
-print("Cut positions:", cut_sites("ACTGACTGA", "ACT"))
+dna = "ACGTACGTAGCTAGCTAGCTAGCTAGC"
+enzyme = "XACT"
+cut_sites = restriction_sites(dna, enzyme)
+print(f"Restriction enzyme sites found at positions: {cut_sites}")
+
+                    
+                    
+                    
