cb371c127c702c77de1fb9cc5764d0130739ea7f
unknown <298521829@qq.com>
2025-05-27 05:59:50 +0100

    second version

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,1 +1,37 @@
 import re
+splice_combination=input("Enter slice combination (GTAG, GCAG, ATAC):")
+output_filename = f"{splice_combination}_spliced_genes.fa"
+
+gene_dict = {}
+seq = ''
+current_gene = None
+
+with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') as input_file:
+    for line in input_file:
+        line = line.strip()
+        if re.search(r'^>',line):
+            if current_gene:
+                gene_dict[current_gene] = seq
+            match = re.search(r'gene:(\S+)', line)
+            if match:
+                current_gene = match.group(1)
+            else:
+                current_gene = line[1:].split()[0]  
+            seq = ''
+        else:
+            seq += line
+    if current_gene:
+        gene_dict[current_gene] = seq
+
+with open(output_filename, 'w') as output_file:
+    for gene_name, gene_seq in gene_dict.items():
+        tata_matches = re.findall(r'TATA[AT]A[AT]', gene_seq)
+        tata_count = len(tata_matches)
+        if tata_count == 0:
+            continue
+
+        if splice_combination in gene_seq:
+            output_file.write(f">{gene_name} TATA_count:{tata_count}\n")
+            output_file.write(f"{gene_seq}\n")
+
+print(f"Output file: {output_filename}")
